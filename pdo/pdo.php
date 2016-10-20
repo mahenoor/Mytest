224471401cd89579d8cf905db3c8be2931db2f9f
mahenoor <mahe.noor@compassitesinc.com>
2016-10-20 09:16:01 +0530

    Update pdo.php

--- pdo/pdo.php
+++ pdo/pdo.php
@@ -1,14 +1,6 @@
 <?php
+require 'functions.php';
 require 'Task.php';
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname = today', 'root', 'compass');
-    echo "connected";
-}
-catch (PDOException $e) {
-    die('could not connect');
-}
-$statement = $pdo->prepare('select * from today');
-$statement->execute();
-var_dump($statement->fetchAll(PDO::FETCH_OBJ));
-require 'linkpdo.php';
+$pdo = connectToDb();
+$tasks = fetchAllTasks($pdo);
 ?>  
