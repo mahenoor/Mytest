b6e7c2cd4c787603a44b1b003d19a3b200856112
mahenoor <mahe.noor@compassitesinc.com>
2016-10-20 09:40:01 +0530

    Update connection.php

--- pdo/connection.php
+++ pdo/connection.php
@@ -4,9 +4,8 @@ class Connection
     public static function make($config)
     {
         try {
-            return new PDO('mysql:host=127.0.0.1;dbname=today', 'root', '');
-            echo "connected";
-        }
+            return new PDO($config['connection'].'; dbname='.$config['name'], $config['username'], $config['password'], $config['options']]);
+            }
         catch (PDOexception $e) {
             die($e->getMessage());
         }
