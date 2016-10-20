3370822899264a5b56b55f9cc237b116a8ae5d2e
mahenoor <mahe.noor@compassitesinc.com>
2016-10-20 09:27:06 +0530

    Update bootstrap1.php

--- pdo/bootstrap1.php
+++ pdo/bootstrap1.php
@@ -1,6 +1,5 @@
 <?php
-$config = require 'config.php';
 require 'connection.php';
 require 'QueryBuilder.php';
-return new QueryBuilder(Connection::make($config['database']));
+return new QueryBuilder(Connection::make());
 ?> 
