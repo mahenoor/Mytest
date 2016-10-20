e82dedd6eb37b6c6c8d0cb5184f58b36be1641b2
mahenoor <mahe.noor@compassitesinc.com>
2016-10-20 09:24:43 +0530

    Update pdo.php

--- pdo/pdo.php
+++ pdo/pdo.php
@@ -1,13 +1,4 @@
 <?php
-require 'bootstrap.php';
-require 'Task.php';
-$pdo   = connection::make();
-$query = new QueryBuilder($pdo);
-$tasks = $query->selectAll('today');
-$tasks = array_map(function($task)
-{
-    return 'foo';
-}, $tasks);
-die(var_dump($tasks));
-require 'linkpdo.php';
+$database = require 'bootstrap.php';
+$tasks = $database->selectAll('today');
 ?>
