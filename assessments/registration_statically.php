cda18b08cdc9a4dea8a050dda2e2a0be79e867b8
mahenoor <mahe.noor@compassitesinc.com>
2016-10-21 11:42:44 +0530

    Update registration_statically.php

--- assessments/registration_statically.php
+++ assessments/registration_statically.php
@@ -1,6 +1,7 @@
 <?php
 class Register
 {
+    /* declaring the variables along with their visibility to be used in the program */
     public $username;
     
     public $email;
@@ -8,36 +9,49 @@ class Register
     public $password;
     
     public $cpassword;
-    
+    /* the below five functions are used to set the $username,$email,$password,$cpassword,$gender*/
+    /* @param \$username
+        @return $username */
     public function username($username)
     {
         $this->username = $username;
     }
+    /* @param \$email
+       @return $email */
     public function email($email)
     {
         $this->email = $email;
-
     }
+    /* @param \$password
+       @return $password */
     public function password($password)
     {
         $this->password = $password;
     }
+    /* @param \$cpassword
+       @return $cpassword */
     public function cpassword($cpassword)
     {
         $this->cpassword = $cpassword;
     }
+    /* @param \$password
+       @return $password if condition is true else returns the error statemnt enclosed in echo */
     public function validate1($password)
     {
         if(!preg_match("/[a-z]{6}/", $password)){
             die('password should be more than 6 characters');
         }
     }
+    /* @param \$cpassword
+       @return $cpassword if condition is true else returns the error statemnt enclosed in echo */
     public function validate2($cpassword)
     {
         if(!preg_match("/[a-z]{6}/", $cpassword)){
             die('cpassword should be more than 6 characters');
         }
     }
+     /* @param \$password,$cpassword
+        @return $password,$cpassword if condition is true else returns the error statemnt enclosed in echo */
     public function validate3($password,$cpassword)
     {
         if($password == $cpassword){
