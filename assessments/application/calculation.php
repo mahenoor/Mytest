ad18e143659243b6479e71dc92508ebcd4548f3a
mahenoor <mahe.noor@compassitesinc.com>
2016-12-06 11:17:20 +0530

    modified the function such that total and percentage can be calculated dynamically in calculation.php file

--- assessments/application/calculation.php
+++ assessments/application/calculation.php
@@ -1,12 +1,14 @@
 <?php
 function total($subject1, $subject2, $subject3) 
 {
-	$total = $subject1 + $subject2 + $subject3;
+	$values = array($subject1 , $subject2 ,$subject3);
+	$total = array_sum($values);
 	return $total;
 }
-function percentage($subject1, $subject2, $subject3) 
+function percentage($subject1, $subject2, $subject3)
 {
-	$percentage = (($subject1 + $subject2 + $subject3) / 3); 
- 	return $percentage;
+	$values = array($subject1 , $subject2 ,$subject3);
+	$percent = array_sum($values) / count($values);
+	return $percent;
 }
 ?>
