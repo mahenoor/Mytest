4e492f3b44201c40ab6f9e08f36d380718af8f54
mahenoor <mahe.noor@compassitesinc.com>
2016-12-06 16:05:06 +0530

    modifying createStudentRecord.php

--- assessments/application/CreateStudentRecord.php
+++ assessments/application/CreateStudentRecord.php
@@ -17,74 +17,98 @@ class StudentRecord
     public $subject3Error;
     public function studentName($studentName)
     {
-        if(empty($studentName) || !preg_match("/^['a-z']{3,9}$/",$studentName) {
-            $studentNameError = "***please enter student name"; 
-        } else {
-            $this->$studentName = $studentName;
-        }
+        $this->$studentName = $studentName;
     }
     public function department($department)
     {
-        if (empty($department)) {
-            $departmentError = "***please enter department";
-        } else {
-            $this->$department = $department;
-        }
+        $this->$department = $department;
     }
     public function gender($gender)
     {
-        if (empty($gender)) {
-            $genderError = "***please enter gender";
-        } else {
-            $this->$gender = $gender;
-        }
+        $this->$gender = $gender;
     }
     public function roll_no($roll_no)
     {
-        if (empty($Roll_no) || !preg_match("/^[a-z0-9]{10}$/", $Roll_no)) {
-            $Roll_noError = "***roll_no can't be empty,must not include capital letters and 
-            must be of only 10 digits";
-        } else {
-            $this->$roll_no = $roll_no;
-        }
+        $this->$roll_no = $roll_no;
     }
     public function subject1($subject1)
     {
-        if (empty($subject1) || !preg_match("/^[0-9]{1,3}$/",  $subject1)) {
-            $subject1Error = "***subject1 can't be empty,must include only digits and with maximum marks being 100";
-        } else {
-            $this->$subject1 = $subject1;
-        }
+        $this->$subject1 = $subject1;
     }
     public function subject2($subject2)
     {
-        if (empty($subject2) || !preg_match("/^[0-9]{1,3}$/",  $subject2)) {
-            $subject2Error = "***subject2 can't be empty,must include only digits and with maximum marks being 100";
-        } else { 
-            $this->$subject2 = $subject2;
-        }
+        $this->$subject2 = $subject2;
     }
     public function subject3($subject3)
     {
-        if (empty($subject3) || !preg_match("/^[0-9]{1,3}$/",  $subject3)) {
-            $subject3Error = "***subject1 can't be empty,must include only digits and with maximum marks being 100";
-        } else {
-            $this->$subject3 = $subject3;
+        $this->$subject3 = $subject3;
+    }
+    public function validateStudentName($studentName)
+    {
+        if (empty($studentName) || !preg_match("/^['a-z']{3,9}$/",$studentName)) {
+            echo "***please enter student name"; 
+        }
+    }
+    public function validateDepartment($department)
+    {
+        if (empty($department)) {
+            echo "***please enter department";
+        }
+    }
+    public function validateGender($gender)
+    {
+        if (empty($gender)) {
+            echo "***please enter gender";
+        }
+    }
+    public function validateRoll_no($roll_no)
+    {
+        if (empty($Roll_no) || !preg_match("/^[a-z0-9]{10}$/", $Roll_no)) {
+            echo "***roll_no can't be empty,must not include capital letters and must be of only 10 digits";
+        }
+    }
+    public function validateSubject1($subject1)
+    {
+        if (empty($subject1) || !preg_match("/^[0-9]{1,3}$/",  $subject1)) {
+            echo "***subject1 can't be empty,must include only digits and with maximum marks being 100";
+        }
+    }
+    public function validateSubject2($subject2)
+    {
+        if (empty($subject2) || !preg_match("/^[0-9]{1,3}$/",  $subject2)) {
+            echo "***subject2 can't be empty,must include only digits and with maximum marks being 100";
+        }
+    }
+    public function validateSubject2($subject2)
+    {
+        if (empty($subject2) || !preg_match("/^[0-9]{1,3}$/",  $subject2)) {
+            echo "***subject2 can't be empty,must include only digits and with maximum marks being 100";
         }
     }
     public function display()
     {
         require 'form.php';
     }
-
-
-
-
-
-
-
-    
-
-
-
-}
+    public function display()
+    {
+        echo "the information is:";
+        echo "the studentName is:" . $this->studentName;
+        echo "the department is:" . $this->department;
+        echo "the gender is:" . $this->gender;
+        echo "the roll_no is:" . $this->roll_no;
+        echo "the subject1 is:" . $this->subject1;
+        echo "the subject2 is:" . $this->subject2;
+        echo "the subject3 is:" . $this->subject3;
+    }
+    $studentRecord = new StudentRecord();
+echo  $studentRecord->display();
+echo  $studentRecord->username($_POST['username']);
+echo $studentRecord->email($_POST['user_email']);
+echo $studentRecord->password($_POST['user_password']);
+echo $studentRecord->cpassword($_POST['user_confirmation_password']);
+echo $studentRecord->gender($_POST['gender']);
+echo $studentRecord->validate1($_POST['user_password']);
+echo $studentRecord->validate2($_POST['user_confirmation_password']);
+echo $studentRecord->validate3($_POST['user_password'], $_POST['user_confirmation_password']);
+echo $studentRecord->college($_POST['college']);
+echo $studentRecord->display1();
